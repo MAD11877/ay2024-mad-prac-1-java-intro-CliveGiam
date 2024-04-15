0b077c9d2bd1a9a386be8be674633792c7e4440d
CliveGiam <148406821+CliveGiam@users.noreply.github.com>
2024-04-15 14:46:49 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Question5
 {
   public static void main(String[] args)
@@ -25,8 +26,44 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+
     Scanner in = new Scanner(System.in);
+    Integer quantity = in.nextInt();
+    int[] array;
+    int[] frequency;
+    for (int i = 0; i < quantity; i++)
+    {
+      Integer next = in.nextInt();
+      array[i] = next;
+    }
+    for (int j = 0; j < quantity; j++)
+    {
+      Integer comparison = array[j];
+      for (int x : array)
+      {
+        Integer count = 0;
+        if (comparison == x)
+        {
+          count += 1;
+        }
+        frequency[j] = count;
+      }
+    }
     
+    Integer index = 0;
+    for (int y = 0; y < quantity; y++)
+    {
+      if (y == 0)
+      {
+        index = frequency[y];
+        continue;
+      }
+      if (frequency[y] > index)
+      {
+        index = frequency[y];
+      }
+    }
+    
+    System.out.println(array[index]);
   }
 }
